d491c30679c25ebf97a8aeaac99d013a01b46f51
marcosbatm <marcosbmentzel@gmail.com>
2026-03-15 01:31:13 -0300

    feat(api): add corresponding summary description to each endpoint

--- src/main.py
+++ src/main.py
@@ -45,38 +45,38 @@ def read_root(product: Product, cartItem: CartItem, cart: Cart):
     service_name = os.getenv("SERVICE_NAME", "FastAPI")
     return {"Hello": "World", "From": service_name}
 
-@app.post("/products")
+@app.post("/products", summary="Create a new product")
 def write_product(product: CreateProductRequest):
     return {"message": "Endpoint to create a new product"}
 
-@app.get("/products")
+@app.get("/products", summary="Retrieve all products")
 def read_products():
     return {"message": "Endpoint to read all products"}
 
-@app.get("/products/{id}")
+@app.get("/products/{id}", summary="Retrieve a product by ID")
 def read_product(id: int):
     return {"message": "Endpoint to read a specific product by id", "id": id}
 
-@app.put("/products/{id}")
+@app.put("/products/{id}", summary="Full update of a product by ID")
 def update_product(id: int, product: UpdateProductRequest):
     return {"message": "Endpoint to update a specific product by id", "id": id}
 
-@app.delete("/products/{id}")
+@app.delete("/products/{id}", summary="Delete a product by ID")
 def delete_product(id: int):
     return {"message": "Endpoint to delete a specific product by id", "id": id}
 
-@app.get("/cart/{userId}")
+@app.get("/cart/{userId}", summary="Get user's cart")
 def read_cart(userId: int):
     return {"message": "Endpoint to read the cart for a specific user by userId", "userId": userId}
 
-@app.delete("/cart/{userId}")
+@app.delete("/cart/{userId}", summary="Wipe a user's cart")
 def clear_cart(userId: int):
     return {"message": "Endpoint to clear the cart for a specific user by userId", "userId": userId}
 
-@app.post("/cart/{userId}/items")
+@app.post("/cart/{userId}/items", summary="Add product to user's cart")
 def add_item_to_cart(userId: int, item: AddCartRequest):
     return {"message": "Endpoint to add an item to the cart for a specific user by userId", "userId": userId}
 
-@app.delete("/cart/{userId}/items/{cartItemId}")
+@app.delete("/cart/{userId}/items/{cartItemId}", summary="Delete an item from a user's cart")
 def remove_item_from_cart(userId: int, cartItemId: int):
     return {"message": "Endpoint to remove an item from the cart for a specific user by userId and cartItemId", "userId": userId, "cartItemId": cartItemId}
