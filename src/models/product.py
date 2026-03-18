f5314c9b83f46c5b02f29464cb8a2868a08c68e2
marcosbatm <marcosbmentzel@gmail.com>
2026-03-18 03:57:32 -0300

    feat(api): remove old Models, define new ones. Only products for now

---
+++ src/models/product.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+
+class BaseProduct(BaseModel):
+    sellerId: int
+    title: str
+    description: str
+    price: float = Field(ge=0.01)
+
+
+class CreateProductRequest(BaseProduct):
+    pass
+
+
+class Product(BaseProduct):
+    id: int
+    price: float = Field(
+        ge=0.01, description="Price in USD, rounded to 2 decimal places"
+    )
+    createdAt: datetime
+    updatedAt: datetime
+
+
+class CreateProductResponse(BaseModel):
+    data: Product
+
+
+class GetProductsResponse(BaseModel):
+    data: list[Product]
